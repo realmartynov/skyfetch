b959c018b267969500b615e5124bae4ff3b47761
realcashwave <realcashwave@gmail.com>
2025-05-02 22:43:00 +0400

    initial commit

---
+++ src/main/java/com/realmartynov/skyfetch/SkyFetchApplication.java
@@ -0,0 +1,13 @@
+package com.realmartynov.skyfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyFetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realmartynov/skyfetch/SkyfetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realmartynov.skyfetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyfetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
